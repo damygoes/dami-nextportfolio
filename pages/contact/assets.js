2e82de88496ec20ba95679c1112d5b24bb53341d
Damilola Bada <90965007+damygoes@users.noreply.github.com>
2022-07-25 19:27:54 +0200

    assets.js file removed - didn't allow deploy to run

--- pages/contact/assets.js
+++
@@ -1,7 +0,0 @@
-export { default as LinkedIn } from "../../assets/linkedin-app.svg";
-export { default as Email } from "../../assets/email-address.svg";
-export { default as Twitter } from "../../assets/twitter-app.svg";
-export { default as Instagram } from "../../assets/ig-instagram.svg";
-export { default as Github } from "../../assets/github.svg";
-export { default as ContactImage } from "../../assets/contact-image.jpg";
-export { default as ContactImagePNG } from "../../assets/contact-png.png";
