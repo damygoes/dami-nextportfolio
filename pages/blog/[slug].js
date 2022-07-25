12fd859d4e4c53d0192a7b2bcbe1192f02569a0c
Damilola Bada <90965007+damygoes@users.noreply.github.com>
2022-07-25 09:43:13 +0200

    Image component fixed on blog page

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -69,7 +69,7 @@ const BlogPost = ({ blog }) => {
 			[BLOCKS.EMBEDDED_ASSET]: (node, children) => {
 				// render the EMBEDDED_ASSET as you need
 				return (
-					<img
+					<Image
 						src={`https://${node.data.target.fields.file.url}`}
 						height={node.data.target.fields.file.details.image.height}
 						width={node.data.target.fields.file.details.image.width}
