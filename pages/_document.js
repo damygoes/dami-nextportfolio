42a256af563921cc57560db111ec8efe7e564dd9
Damilola Bada <90965007+damygoes@users.noreply.github.com>
2022-07-31 14:37:28 +0200

    added custom document

---
+++ pages/_document.js
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+	return (
+		<Html>
+			<Head>
+				<title>Damilola Bada | Web Developer </title>
+			</Head>
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
+}
