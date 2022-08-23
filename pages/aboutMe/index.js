3572bc0c968f33052cde4c06b9c14f45020d789a
Damilola Bada <90965007+damygoes@users.noreply.github.com>
2022-08-23 16:02:00 +0200

    about me section re-designed

--- pages/aboutMe/index.js
+++ pages/aboutMe/index.js
@@ -9,14 +9,12 @@ import Twitter from "../../assets/twitter-app.svg";
 import Instagram from "../../assets/ig-instagram.svg";
 import Github from "../../assets/github.svg";
 import ME1 from "../../assets/me1.jpg";
-import ME2 from "../../assets/me2.jpg";
-import ME3 from "../../assets/me3.jpg";
-import ME4 from "../../assets/me4.jpg";
-import ME5 from "../../assets/me5.jpg";
-
 import Portrait from "../../assets/portrait.png";
-
 import styles from "./Aboutme.module.css";
+// import ME2 from "../../assets/me2.jpg";
+// import ME3 from "../../assets/me3.jpg";
+// import ME4 from "../../assets/me4.jpg";
+// import ME5 from "../../assets/me5.jpg";
 
 const Aboutme = () => {
 	return (
@@ -27,126 +25,107 @@ const Aboutme = () => {
 				</h3>
 				<div className="xyz-in" xyz="fade in-right delay-1 duration-2"></div>
 			</div>
-			<section className={styles.upper}>
-				<div className={styles.left}>
-					<p className="xyz-in" xyz="fade in-left delay-1 duration-2">
-						My name is <span>Damilola Bada</span>. I have a background in
-						Environmental Science and experience in marketing and selling a brand.
-						These have influenced my
-						<span>curious, creative and competitive approach</span> to solving
-						problems.
+			<section className={styles.main}>
+				<div className={styles.content}>
+					<p className={styles.text}>
+						My name is <span>Damilola Bada</span>. I am a motivated and goal-oriented
+						front-end developer. I am passionate about building intuitive,
+						user-friendly and yet creative web applications. I am focused on providing
+						pixel-perfect designs and writing clean code. In addition, I have a
+						curious personality that makes me crave knowledge and self-development and
+						learn from every challenge I encounter.
 					</p>
-					<p className="xyz-in" xyz="fade in-left delay-1 duration-2">
-						As a former Marketing Manager, I learned that the most efficient way to
-						grow a brand&apos;s influence and profit is by providing a great customer
-						and <span>user experience</span>. With this strategy; combined with my
-						data analysis skills, I increased my team&apos;s profit by 21%. My
-						continued interest in <span>delivering top-notch user experience</span>,
-						and my <span>passion for learning and development</span>, led me to pursue
-						<span>web development</span> as a career.
+					<p className={styles.text}>
+						With an environmental science background and experience in the finance
+						industry, I have learned how to work in high-pressure situations, deal
+						with demanding clients and requests, manage available resources, provide
+						creative solutions, contribute to teamwork and collaborate with others.
 					</p>
-					<p className="xyz-in" xyz="fade in-left delay-1 duration-2">
-						I have a great passion for building
-						<span>mordern, creative and dynamic</span>
-						websites, applications and products for the web. I also enjoy the
-						challenge of <span>learning and working with different technologies</span>
-						because, there is no one tool that fits all situation.
-					</p>
-					<p className="xyz-in" xyz="fade in-left delay-1 duration-2">
-						When I&apos;m not working, I am either riding my race bike, gracing events
-						as a Disc Jockey (DJ), playing video games or spending time with my wife
-						and daughter.
-					</p>
-
-					<div className={styles.socials}>
-						<p className="xyz-in" xyz="fade in-left delay-1 duration-2">
-							check out my socials:
-						</p>
-						<div className={styles.iconContainer}>
-							<Link href="https://www.linkedin.com/in/damilolabada/">
-								<a
-									target="_blank"
-									rel="noreferrer"
-									className="xyz-in"
-									xyz="fade front flip-left delay-4 duration-2"
-								>
-									<Image src={LinkedIn} alt="LinkedIn Logo" />
-								</a>
-							</Link>
-						</div>
-						<div className={styles.iconContainer}>
-							<Link href="https://github.com/damygoes">
-								<a
-									target="_blank"
-									rel="noreferrer"
-									className="xyz-in"
-									xyz="fade front flip-left delay-6 duration-2"
-								>
-									<Image src={Github} alt="Github Logo" />
-								</a>
-							</Link>
-						</div>
-						<div className={styles.iconContainer}>
-							<Link href="https://twitter.com/damygoes">
-								<a
-									target="_blank"
-									rel="noreferrer"
-									className="xyz-in"
-									xyz="fade front flip-left delay-8 duration-2"
-								>
-									<Image src={Twitter} alt="TwitterLogo" />
-								</a>
-							</Link>
-						</div>
-						<div className={styles.iconContainer}>
-							<Link href="https://www.instagram.com/damy.goes/">
-								<a
-									target="_blank"
-									rel="noreferrer"
-									className="xyz-in"
-									xyz="fade front flip-left delay-10 duration-2"
-								>
-									<Image src={Instagram} alt="Instagram Logo" />
-								</a>
-							</Link>
-						</div>
-					</div>
 				</div>
-				<div className={styles.right}>
-					<div className={styles.card}>
-						<Image
-							src={ME5}
-							alt="Dami's Portrait"
-							className="xyz-in"
-							xyz="fade flip-left perspective-5 delay-1 duration-5"
-						/>
-					</div>
-					<div className={styles.card}>
-						<Image
-							src={ME1}
-							alt="Dami's Portrait"
-							className="xyz-in"
-							xyz="fade flip-left perspective-5 delay-3 duration-5"
-						/>
-					</div>
-					<div className={styles.card}>
-						<Image
-							src={ME2}
-							alt="Dami's Portrait"
-							className="xyz-in"
-							xyz="fade flip-right perspective-5 delay-7 duration-5"
-						/>
-					</div>
-					<div className={styles.card}>
-						<Image
-							src={ME3}
-							alt="Dami's Portrait"
-							className="xyz-in"
-							xyz="fade flip-left perspective-5 delay-5 duration-5"
-						/>
-					</div>
+				<div className={styles.portraitOne}>
+					<Image
+						src={Portrait}
+						alt="Dami's Portrait"
+						layout="responsive"
+						objectFit="contain"
+						className="xyz-in"
+						xyz="fade flip-left perspective-5 delay-1 duration-5"
+					/>
 				</div>
+				<p className={styles.discoveryText + " " + styles.text}>
+					I discovered web development while exploring how to create a journal for my
+					cycling adventures. As a result, I decided to change my career path and use
+					my transferable skills to pursue a career as a Front-end Developer.
+				</p>
+				<div className={styles.portraitTwo}>
+					<Image
+						src={ME1}
+						alt="Dami's Portrait"
+						layout="responsive"
+						objectFit="contain"
+						className="xyz-in"
+						xyz="fade flip-left perspective-5 delay-1 duration-5"
+					/>
+				</div>
+				<p className={styles.goalText + " " + styles.text}>
+					I aim to continue learning and refining my skills while seeking
+					collaboration with other developers and opportunities to work in an
+					inclusive team that promotes professional growth, empathy and creativity.
+				</p>
 			</section>
+			<div className={styles.socials}>
+				<p className="xyz-in" xyz="fade in-left delay-1 duration-2">
+					check out my socials:
+				</p>
+				<div className={styles.iconContainer}>
+					<Link href="https://www.linkedin.com/in/damilolabada/">
+						<a
+							target="_blank"
+							rel="noreferrer"
+							className="xyz-in"
+							xyz="fade front flip-left delay-4 duration-2"
+						>
+							<Image src={LinkedIn} alt="LinkedIn Logo" />
+						</a>
+					</Link>
+				</div>
+				<div className={styles.iconContainer}>
+					<Link href="https://github.com/damygoes">
+						<a
+							target="_blank"
+							rel="noreferrer"
+							className="xyz-in"
+							xyz="fade front flip-left delay-6 duration-2"
+						>
+							<Image src={Github} alt="Github Logo" />
+						</a>
+					</Link>
+				</div>
+				<div className={styles.iconContainer}>
+					<Link href="https://twitter.com/damygoes">
+						<a
+							target="_blank"
+							rel="noreferrer"
+							className="xyz-in"
+							xyz="fade front flip-left delay-8 duration-2"
+						>
+							<Image src={Twitter} alt="TwitterLogo" />
+						</a>
+					</Link>
+				</div>
+				<div className={styles.iconContainer}>
+					<Link href="https://www.instagram.com/damy.goes/">
+						<a
+							target="_blank"
+							rel="noreferrer"
+							className="xyz-in"
+							xyz="fade front flip-left delay-10 duration-2"
+						>
+							<Image src={Instagram} alt="Instagram Logo" />
+						</a>
+					</Link>
+				</div>
+			</div>
 			<Link href={"/projects"}>
 				<div
 					className={styles.pageBackNav + " " + "xyz-in"}
@@ -156,7 +135,12 @@ const Aboutme = () => {
 					<HiOutlineChevronRight />
 				</div>
 			</Link>
-			{/* <Link href={"/blog"}>
+		</section>
+	);
+};
+
+{
+	/* <Link href={"/blog"}>
 				<div
 					className={styles.pageNav + " " + "xyz-in"}
 					xyz="fade in-right delay-6 duration-5"
@@ -164,9 +148,7 @@ const Aboutme = () => {
 					<HiOutlineChevronLeft /> <p>blog</p> <CgFormatSlash />{" "}
 					<HiOutlineChevronRight />
 				</div>
-			</Link> */}
-		</section>
-	);
-};
+			</Link> */
+}
 
 export default Aboutme;
